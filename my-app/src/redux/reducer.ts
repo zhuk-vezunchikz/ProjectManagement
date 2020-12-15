26cc4f82c74ce00f4f9b64f02cee74e7fbe10d57
zhuk-vezunchikz <vezunchik05011996>
2020-12-15 13:51:24 +0300

    Создание проекта по управлению проектами на React + typescript.

---
+++ my-app/src/redux/reducer.ts
@@ -0,0 +1,12 @@
+import initialState from "./initialState";
+
+
+function reducer(state = initialState, action: any) {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
